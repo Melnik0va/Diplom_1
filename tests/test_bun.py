9add95d8e8c8238811d60947a53ec75568ed7b08
melni_k0va <yulatnx@mail.ru>
2025-12-22 12:58:59 +0300

    Был поправлен test_bun: теперь два отдельных теста на проверку имени и стоимости

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -5,7 +5,11 @@ from praktikum.bun import Bun
 class TestBun: 
 
     @pytest.mark.parametrize('name, price', [('Белая', 50), ('Черная', 100), ('Зеленая', 150) ])
-    def test_class_Bun(self, name, price): 
+    def test_class_Bun_get_name(self, name, price): 
         bun = Bun(name, price)
         assert bun.get_name() == name
+
+    @pytest.mark.parametrize('name, price', [('Белая', 50), ('Черная', 100), ('Зеленая', 150) ])
+    def test_class_Bun_get_price(self, name, price): 
+        bun = Bun(name, price)
         assert bun.get_price() == price
