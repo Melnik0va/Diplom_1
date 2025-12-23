c1cd772153ca06626366620a10867b9691b56600
melni_k0va <yulatnx@mail.ru>
2025-12-23 22:16:53 +0300

    Был поправлен test_ingredient: теперь три отдельных теста на проверку имени, стоимости и типа ингредиента

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -1,15 +1,17 @@
-import pytest 
 from praktikum.ingredient import Ingredient
 from praktikum.ingredient_types import INGREDIENT_TYPE_FILLING, INGREDIENT_TYPE_SAUCE
+from data import ingredient_info
 
-class TestIngredient: 
+class TestIngredient:
 
-    @pytest.mark.parametrize('ingredient_type, name, price', [
-        (INGREDIENT_TYPE_FILLING, 'Мясо бессмертных малюсков', 1337),
-        (INGREDIENT_TYPE_SAUCE, 'Соус Spicy-X', 90)
-    ])
-    def test_ingridient_init(self, ingredient_type, name, price): 
-        ingredient = Ingredient(ingredient_type, name, price)
-        assert ingredient.get_price() == price
-        assert ingredient.get_name() == name
-        assert ingredient.get_type() == ingredient_type
+    def test_ingridient_init_get_price(self):
+        ingredient = Ingredient(INGREDIENT_TYPE_FILLING, ingredient_info['name'], ingredient_info['price'])
+        assert ingredient.get_price() == ingredient_info['price']
+
+    def test_ingridient_init_get_name(self):
+        ingredient = Ingredient(INGREDIENT_TYPE_FILLING, ingredient_info['name'], ingredient_info['price'])
+        assert ingredient.get_name() == ingredient_info['name']
+
+    def test_ingridient_init_get_ingredient_type(self):
+        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, ingredient_info['name'], ingredient_info['price'])
+        assert ingredient.get_type() == INGREDIENT_TYPE_SAUCE
